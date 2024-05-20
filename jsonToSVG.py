f204785601c531183a87f183376a626d311fed60
Arthur Masson <arthur.masson@inria.fr>
2024-05-20 23:27:55 +0200

    add paperWidth|Height & margin

--- jsonToSVG.py
+++ jsonToSVG.py
@@ -3,12 +3,21 @@ import json
 import argparse
 from scipy.spatial import KDTree
 import svgwrite
+import math
 
 parser = argparse.ArgumentParser('JSON to SVG', description='Converts a json file generated with the space-filling-curve webpage to svg.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
 parser.add_argument('-i', '--input', help='Input json', required=True)
+parser.add_argument('-pw', '--paperWidth', help='Paper width in mm.', default=500, type=float)
+parser.add_argument('-ph', '--paperHeight', help='Paper height in mm.', default=650, type=float)
+# parser.add_argument('-pw', '--paperWidth', help='Paper width in mm, compted from height if None (by keeping the drawing aspect ratio). If both height and width are None, width will be set to 500mm and height will be computed accordingly.', default=None, type=float)
+# parser.add_argument('-ph', '--paperHeight', help='Paper height in mm, computed from width if None.', default=None, type=float)
 # parser.add_argument('-s', '--sorted', action='store_true', help='Path is sorted. Use to skip points sorting.')
-parser.add_argument('-m', '--margin', type=float, help='Margin (defined as a percentage of the max side of the drawing bounding box)', default=0.01)
+parser.add_argument('-m', '--margin', type=float, help='Margin in mm', default=30)
+parser.add_argument('-vc', '--verticalCorrection', type=float, help='Vertical correction (to compensate the shilouette offset)', default=-7)
+parser.add_argument('-hc', '--heightCorrection', type=float, help='Height correction (to compensate the shilouette scale)', default=0)
+parser.add_argument('-df', '--drawFrames', action='store_true', help='Draw the drawing bounding box and the paper rectangle')
+parser.add_argument('-sbb', '--skipBoundingBox', action='store_true', help='Do not output the bounding box svg')
 
 args = parser.parse_args()
 
@@ -32,6 +41,13 @@ def areClose(p1, p2):
 # if not args.sorted:
 width = maxX - minX
 height = maxY - minY
+
+# if args.paperWidth is None and args.paperHeight is None:
+#     args.paperWidth = 500
+# if args.paperWidth is None:
+#     args.paperWidth = args.paperHeight * width / height
+# if args.paperHeight is None:
+#     args.paperHeight = args.paperWidth * height / width
 
 # tree = quads.QuadTree((minX+width/2, minY+height/2), 2*width, 2*height)
 ps = [[path[0], path[-1]] for path in data]
@@ -60,14 +76,65 @@ while n<len(data):
 print('Created path.')
 data = finalData
 
+paperRatio = args.paperWidth / args.paperHeight
+drawingRatio = width / height
 
-svgName = Path(args.input).with_suffix('.svg')
-frame_width, frame_height = 1000, 1000
+# if paperRatio > drawingRatio: # paper is wider compared to drawing: margin are defined vertically
+#     drawingHeightMM = args.paperHeight - 2 * args.margin
+#     mmToUnit = height / drawingHeightMM
+#     drawingMarginV = args.margin * mmToUnit
+#     drawingWidthMM = drawingHeightMM * width / height
+#     drawingMarginHMM = (args.paperWidth - drawingWidthMM) / 2
+#     drawingMarginH = drawingMarginHMM * mmToUnit
+# else: # paper is taller compared to drawing: margin are defined horizontally
+#     drawingWidthMM = args.paperWidth - 2 * args.margin
+#     mmToUnit = width / drawingWidthMM
+#     drawingMarginH = args.margin * mmToUnit
+#     drawingHeightMM = drawingWidthMM * height / width
+#     drawingMarginVMM = (args.paperHeight - drawingHeightMM) / 2
+#     drawingMarginV = drawingMarginVMM * mmToUnit
 
-margin = args.margin * max(maxX, maxY)
-viewBox = f'{minX - margin:.2} {minY - margin:.2} {maxX + margin:.2} {maxY + margin:.2}'
-drawing = svgwrite.Drawing(svgName, width=frame_width, height=frame_height, viewBox=viewBox)
+if paperRatio > drawingRatio: # paper is wider compared to drawing: margin are defined vertically
+    mmToUnit = height / (args.paperHeight - 2 * args.margin)
+    marginV = args.margin * mmToUnit
+    totalWidth = args.paperWidth * mmToUnit
+    marginH = (totalWidth - height) / 2
+else: # paper is taller compared to drawing: margin are defined horizontally
+    mmToUnit = width / (args.paperWidth - 2 * args.margin)
+    marginH = args.margin * mmToUnit
+    totalHeight = args.paperHeight * mmToUnit
+    marginV = (totalHeight - height) / 2
 
-drawing.add(drawing.polyline(data, fill='none', stroke='black', stroke_width=0.5))
+verticalCorrection = args.verticalCorrection * mmToUnit
+heightCorrection = args.heightCorrection * mmToUnit
+heightMM = height / mmToUnit
+hc = ((args.paperHeight * (heightMM + args.heightCorrection) / heightMM) - args.paperHeight) * mmToUnit
+
+input = Path(args.input)
+svgName = input.with_suffix('.svg')
+
+viewBox = dict(x=minX - marginH, y=minY - marginV - verticalCorrection, width=maxX + 2 * marginH, height=maxY + 2 * marginV + hc)
+
+viewBoxString = f'{viewBox["x"]:.5} {viewBox["y"]:.5} {viewBox["width"]:.5} {viewBox["height"]:.5}'
+drawing = svgwrite.Drawing(svgName, size=(f'{args.paperWidth}mm', f'{args.paperHeight}mm'), viewBox=viewBoxString)
+
+if args.drawFrames:
+    drawing.add(drawing.rect(insert=(viewBox["x"], viewBox["y"]), size=(viewBox["width"], viewBox["height"]), fill='none', stroke='red', stroke_width=1))
+    drawing.add(drawing.rect(insert=(minX, minY-verticalCorrection), size=(width, height * (viewBox["height"] + hc) / viewBox["height"] ), fill='none', stroke='green', stroke_width=1))
+    drawing.add(drawing.rect(insert=(minX, minY), size=(width, height), fill='none', stroke='blue', stroke_width=1))
+
+nPointsMax = 32678
+nChunk = math.ceil(len(data) / nPointsMax)
+
+for i in range(nChunk):
+    beginPointIndex = i*nPointsMax
+    endPointIndex = min(len(data), (i+1)*nPointsMax+1)
+    drawing.add(drawing.polyline(data[beginPointIndex:endPointIndex], fill='none', stroke='black', stroke_width=0.5))
 
 drawing.save()
+
+if not args.skipBoundingBox:
+
+    rectangle = svgwrite.Drawing(input.parent / f'{input.stem}_bounding_box.svg', size=(f'{args.paperWidth}mm', f'{args.paperHeight}mm'), viewBox=viewBoxString)
+    rectangle.add(rectangle.rect(insert=(minX, minY), size=(width, height), fill='none', stroke='blue', stroke_width=1))
+    rectangle.save()
